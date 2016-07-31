c2e5134aebbc91e84de92f148215acb83c240ce6
fengjunpku <fengjun2013@pku.edu.cn>
2016-08-01 01:59:51 +0800

    templates mode

--- api/mmQuery.py
+++ api/mmQuery.py
@@ -16,28 +16,15 @@ class mmQuery:
   def POST(self):
     data = web.input()
     if data['page']=='home':
-      return render.jsframe(3)
+      urls=["?id=ma5","?id=close"]
+      return render.jsframe(len(urls),urls)
     else:
       return data['page']
 
-
 class mmDraw:
   def GET(self):
-    content = '''
-    <script src="https://root.cern.ch/js/latest/scripts/JSRootCore.min.js"></script>
-    <script type='text/javascript'>
-     // absolute file path can be used as well
-     var filename = "../data/hist.root";
-     JSROOT.OpenFile(filename, function(file) {
-        file.ReadObject("h1;1", function(obj) {
-           JSROOT.draw("drawing", obj, "");
-        });
-     });
-    </script>
-    <div id="drawing" style="width:100%; height:100%"></div>
-    '''
-    return content
-    
+    url_data = web.input()
+    return render.jsroot(url_data.id)
 
 application = web.application(urls, globals()).wsgifunc()
 
